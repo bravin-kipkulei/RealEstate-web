22befd92f66b101aebeb27074dd647a508b8a158
bravinkipkulei <bravinkipkulei@gmail.com>
2023-09-12 12:26:00 +0300

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div classname="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
+   
