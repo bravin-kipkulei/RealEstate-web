568763ba6fb12f70d97f22742973924844cf8f02
bravinkipkulei <bravinkipkulei@gmail.com>
2023-10-04 23:31:59 +0300

    trying better import of images to help during production

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import "./Companies.css"
+      //images
+import prologis from "../../Assets/prologis.png"
+import tower from "../../Assets/tower.png"
+import equinix from "../../Assets/equinix.png"
+import realty from "../../Assets/realty.png"
+
 const Companies = () => {
   return (
     <section className='c-wrapper'>
         <div className="paddings innerWidth flexCenter c-container">
-               <img src="src/Assets/prologis.png" alt="" />
-               <img src="src/Assets/tower.png" alt="" />
-               <img src="src/Assets/equinix.png" alt="" />
-               <img src="src/Assets/realty.png" alt="" />
+               <img src={prologis} alt="" />
+               <img src={tower} alt="" />
+               <img src={equinix} alt="" />
+               <img src={realty} alt="" />
         </div>
     </section>
   )
